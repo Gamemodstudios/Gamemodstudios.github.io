4ef46046ca96c54abdf397334a76d0d157244ef0
axelsson09 <william09.axelsson@gmail.com>
2023-12-09 10:43:52 +0100

    Add files via upload

---
+++ website/game mod studios/persions.sql
@@ -0,0 +1,9 @@
+CREATE TABLE [dbo].[persions]
+(
+  [Id] INT NOT NULL PRIMARY KEY
+      PersonID int,
+    LastName varchar(255),
+    FirstName varchar(255),
+    Address varchar(255),
+    City varchar(255)
+)
